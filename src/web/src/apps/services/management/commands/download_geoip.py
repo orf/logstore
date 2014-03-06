1448a76271a94086b0067bbe3723a89ef325e6e8
orf <tom@tomforb.es>
2014-03-06 16:03:37 +0000

    Added a command to download MaxMinds GeoIP database

---
+++ src/web/src/apps/services/management/commands/download_geoip.py
@@ -0,0 +1,27 @@
+# Downloads and extracts
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import os
+import gzip
+import urllib
+
+
+class Command(BaseCommand):
+    help = "Downloads and extracts the GeoIP database"
+
+    def handle(self, *args, **options):
+        target_dir = settings.GEOIP_DATABASE_DIR
+        download_file = os.path.join(target_dir, "GeoIP.dat.gz")
+        extracted_file = os.path.join(target_dir, "GeoIP.dat")
+
+        urllib.urlretrieve("http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz",
+                           download_file)
+        self.stdout.write("Downloaded GeoIP database, extracting")
+        gzip_file = gzip.open(download_file)
+
+        with open(extracted_file, "wb") as extract_fd:
+            extract_fd.write(gzip_file.read())
+        gzip_file.close()
+        self.stdout.write("Extracted to %s" % extracted_file)
+        os.remove(download_file)
