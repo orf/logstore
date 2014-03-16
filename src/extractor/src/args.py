58d5e3e4ca64cf7366c35cb9c759d6a68b5772ae
orf <tom@tomforb.es>
2014-03-16 07:13:07 +0000

    fix parse_args

--- src/extractor/src/args.py
+++ src/extractor/src/args.py
@@ -6,8 +6,8 @@ def parse_arguments(args):
     returner = {}
 
     for i, arg in enumerate(shlex.split(args)):
-        returner[i] = args
-        match = re.match('(?P<name>[a-zA-Z]*):"(?P<value>.*)"', args)
+        returner[i] = arg
+        match = re.match('(?P<name>[a-zA-Z]+):(?P<value>.*)', arg)
         if match:
             groupdict = match.groupdict()
             returner[groupdict["name"]] = groupdict["value"]
