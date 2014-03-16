d839063d87bfaa984b80c770e3ba3921328c0672
orf <tom@tomforb.es>
2014-03-16 05:45:36 +0000

    Added a strptime transformer

--- src/extractor/src/transformers.py
+++ src/extractor/src/transformers.py
@@ -1,5 +1,6 @@
 from .register import registry
 from .base import Transformer
+from datetime import datetime
 
 
 @registry.add_transformer("remove")
@@ -24,3 +25,12 @@ class LowerCase(Transformer):
 class UpperCase(Transformer):
     def transform(self, value):
         return value.upper(), None
+
+
+@registry.add_transformer("time format")
+class TimeFormat(Transformer):
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def transform(self, value):
+        return datetime.strptime(value, self.pattern)
